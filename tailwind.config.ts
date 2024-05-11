4ed6660c8ab1b121502daeb526a2bab57f103ef2
Franco Taaber <franco@redwall.ee>
2024-05-12 02:57:21 +0300

    Creating styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        cream: '#F6F5F2',
+        beige: '#F0EBE3',
+        pink: '#F3D0D7',
+        offwhite: '#FFEFEF'
       },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translate(0, 0) rotate(0)' },
+          '50%': { transform: 'translate(30px, -30px) rotate(360deg)' }
+        },
+        blob: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.2)' }
+        },
+      },
+      animation: {
+        float: 'float 6s ease-in-out infinite',
+        blob: 'blob 7s infinite'
+      }
     },
   },
-  plugins: [],
 };
+
 export default config;
